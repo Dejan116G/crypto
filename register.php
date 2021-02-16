9d24a8c7d47b71d38ba83c5d98e05f969ca134dc
Dejan116G <dejan.gorisek123@gmail.com>
2021-02-16 19:37:34 +0100

    login pripravljen, nedokoncan

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
 
 <section class="page-section" id="contact">
     <div class="container">
-        <!-- Contact Section Heading-->
+        <!-- Registration Section Heading-->
         <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Registracija</h2>
         <!-- Icon Divider-->
         <div class="divider-custom">
@@ -12,7 +12,7 @@
             <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
             <div class="divider-custom-line"></div>
         </div>
-        <!-- Contact Section Form-->
+        <!-- Registration Section Form-->
         <div class="row">
             <div class="col-lg-8 mx-auto">
                 <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19.-->
